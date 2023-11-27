8d67febd21d86261459b32293c108ca69d7d003c
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-11-27 23:06:46 +0700

    Add files via upload

---
+++ SLOWDNS/installsl.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+echo "Installing SLOWDNS by KLMPK" | lolcat
+echo "Progress..." | lolcat
+sleep 3
+cd
+
+ns_domain_cloudflare() {
+	DOMAIN=klmpk-tunneling.my.id
+	DOMAIN_PATH=$(cat /etc/xray/domain)
+	SUB=$(tr </dev/urandom -dc a-z0-9 | head -c7)
+	SUB_DOMAIN=${SUB}.klmpk-tunneling.my.id
+	NS_DOMAIN=ns.${SUB_DOMAIN}
+	CF_ID=andyyuda41@gmail.com
+        CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+	set -euo pipefail
+	IP=$(wget -qO- ipinfo.io/ip)
+	echo "Updating DNS NS for ${NS_DOMAIN}..."
+	ZONE=$(
+		curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+		-H "X-Auth-Email: ${CF_ID}" \
+		-H "X-Auth-Key: ${CF_KEY}" \
+		-H "Content-Type: application/json" | jq -r .result[0].id
+	)
+
+	RECORD=$(
+		curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+		-H "X-Auth-Email: ${CF_ID}" \
+		-H "X-Auth-Key: ${CF_KEY}" \
+		-H "Content-Type: application/json" | jq -r .result[0].id
+	)
+
+	if [[ "${#RECORD}" -le 10 ]]; then
+		RECORD=$(
+			curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+			-H "X-Auth-Email: ${CF_ID}" \
+			-H "X-Auth-Key: ${CF_KEY}" \
+			-H "Content-Type: application/json" \
+			--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DOMAIN_PATH}'","proxied":false}' | jq -r .result.id
+		)
+	fi
+
+	RESULT=$(
+		curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+		-H "X-Auth-Email: ${CF_ID}" \
+		-H "X-Auth-Key: ${CF_KEY}" \
+		-H "Content-Type: application/json" \
+		--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DOMAIN_PATH}'","proxied":false}'
+	)
+	echo $NS_DOMAIN >/etc/xray/dns
+ 
+}
+
+setup_dnstt() {
+	cd
+	mkdir -p /etc/slowdns
+	wget -O dnstt-server "https://raw.githubusercontent.com/valkry7/LT/MONSTER/SLOWDNS/dnstt-server" >/dev/null 2>&1
+	chmod +x dnstt-server >/dev/null 2>&1
+	wget -O dnstt-client "https://raw.githubusercontent.com/valkry7/LT/MONSTER/SLOWDNS/dnstt-client" >/dev/null 2>&1
+	chmod +x dnstt-client >/dev/null 2>&1
+	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
+	chmod +x *
+	mv * /etc/slowdns
+	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/valkry7/LT/MONSTER/SLOWDNS/client" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/valkry7/LT/MONSTER/SLOWDNS/server" >/dev/null 2>&1
+	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
+	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
+}
+ns_domain_cloudflare
+setup_dnstt
+#start servis client
+systemctl daemon-reload
+systemctl enable client.service
+systemctl start client.service
+systemctl restart client.service
+#start servis server.service
+systemctl daemon-reload
+systemctl enable server.service
+systemctl start server.service
+systemctl restart server.service
+#hapus file installsl.sh
+rm -f installsl.sh
